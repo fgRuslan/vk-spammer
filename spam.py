cb014a339ca7115580f01f5181d6afcf7de1d9b0
Ruslan <ubijca16@gmail.com>
2020-10-10 23:03:49 +0300

    Сделал всё красиво
    ня

--- spam.py
+++ spam.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # Author: https://vk.com/id181265169
+# https://github.com/fgRuslan/vk-spammer
 
 import vk, urllib.request, urllib.error, urllib.parse, json, random, time
 
-username = input("Login:")
-password = input("Password:")
+username = input("Login: ")
+password = input("Password: ")
 
 url = "https://oauth.vk.com/token?grant_type=password&client_id=3697615&client_secret=AlVXZFMUqyrnABp8ncuU&username=%s&password=%s" % (username, password)
 
@@ -21,8 +22,6 @@ vk = vk.API(session)
 
 foo = ["hi", "2", "3", "fuck", "5"]
 
-# print (foo)
-
 victim = input("User id: ")
 
 r = vk.users.get(user_id = victim, fields = "id", v = 5.73)
@@ -30,13 +29,13 @@ r = r[0]["id"]
 
 victim = r
 
+print("-" * 60)
 while(True):
 	try:
 		time.sleep(random.randint(1,3) + random.randint(1,4))
 		r = vk.messages.send(peer_id = victim, message = random.choice(foo), v = 5.73)
-		print()
 		print("wait...")
 		time.sleep(random.randint(1,2) + random.randint(1,2))
-		print("done  ",random.choice(foo))
+		print("done ", random.choice(foo))
 	except:
 		pass
