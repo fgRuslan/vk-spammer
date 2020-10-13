4a7cab3d125a0ce6a816b0d7dfb16fa2384bbf77
Ruslan Isaev <ubijca16@gmail.com>
2020-10-13 20:25:41 +0300

    Обновил информацию в setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+DESCRIPTION = 'A VK spammer'
+
+try:
+    with open("README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
 setup(
     name='vk-spammer',
-    version='1.0',
+    version='1.01',
+    author='fgRuslan',
+    author_email='ubijca16@gmail.com',
+    url='https://github.com/fgRuslan/vk-spammer',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
     entry_points={
