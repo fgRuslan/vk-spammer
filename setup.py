b28ead2f00e19f992aeb4c985e5e3b2f234f49c5
Ruslan Isaev <ubijca16@gmail.com>
2020-10-14 18:53:49 +0300

    Update setup.py
    Обновил версию

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ LONG_DESC = DESCRIPTION
 
 setup(
     name='vk-spammer',
-    version='1.02.1',
+    version='1.2.1.1',
     author='fgRuslan',
     author_email='ubijca16@gmail.com',
     url='https://github.com/fgRuslan/vk-spammer',
