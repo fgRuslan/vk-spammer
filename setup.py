e274ea4126e9fb5372fdcc32bf75c6f1784a74c6
Ruslan Isaev <ubijca16@gmail.com>
2021-07-07 15:26:07 +0300

    v1.2.2.9
    - Добавлены возможности спама всем друзьям и друзьям, которые в сети
    - Добавлена возможность вводить капчу (либо, если у вас есть ключ от API
      anti-captcha.com, можно это автоматизировать)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESC = long_description
 
 setup(
     name='vk-spammer',
-    version='1.2.2.8',
+    version='1.2.2.9',
     author='fgRuslan',
     author_email='ubijca16@gmail.com',
     url='https://github.com/fgRuslan/vk-spammer',
@@ -20,6 +20,7 @@ setup(
             ['vk-spammer=core.spam:main']
     },
     install_requires=[
-        'vk'
+        'vk_api',
+        'python3_anticaptcha'
     ]
 )
