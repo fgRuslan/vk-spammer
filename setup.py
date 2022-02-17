57ec9d53d01585b4df0f22ef3dc762e92c0e9c4a
Ruslan Isaev <ubijca16@gmail.com>
2022-02-17 13:36:40 +0300

    v1.2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESC = long_description
 
 setup(
     name='vk-spammer',
-    version='1.2.3.9',
+    version='1.2.4',
     author='fgRuslan',
     author_email='ubijca16@gmail.com',
     url='https://github.com/fgRuslan/vk-spammer',
